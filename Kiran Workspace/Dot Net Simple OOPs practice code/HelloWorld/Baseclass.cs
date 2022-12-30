8c1b866de23e3d180721984c0ea3c0c2ba007998
KiranKhadd29 <121477204+KiranKhadd29@users.noreply.github.com>
2022-12-30 17:22:31 +0530

    Simple C# oops concept practice code

---
+++ Kiran Workspace/Dot Net Simple OOPs practice code/HelloWorld/Baseclass.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using HelloWorld;
+
+class Baseclass
+{
+    public  virtual void mahindraCar()
+    {
+        Console.WriteLine("XUV");
+    }
+    static void Main(string[] args)
+    {
+        Basic_datatype test = new Basic_datatype();
+        //test.method1();
+        Loops test2 = new Loops();
+        //test2.loop();
+        Car mahindraCar = new Car();  // Create an object of the Car Class (this will call the constructor)
+                                      //Console.WriteLine(mahindraCar.Model);
+
+        Baseclass Mcar1 = new Baseclass();
+        Baseclass Mcar2 = new PolyCar();
+        Baseclass Mcar3 = new Polytruck();
+
+        Mcar1.mahindraCar();
+        Mcar2.mahindraCar();
+        Mcar3.mahindraCar();
+
+        PracticeExamples ex = new PracticeExamples();
+        //ex.swapNumber();
+        //ex.primeNum();
+
+    }
+
+
+}
+
+
---
+++ Kiran Workspace/Dot Net Simple OOPs practice code/HelloWorld/Basic datatype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+
+    class Basic_datatype :  Baseclass
+    {
+        public void method1()
+        {
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+        }
+    }
+}
+ 
