c4d3429204c91b2900a5c88e34bfe847f36f2af2
KiranKhadd29 <121477204+KiranKhadd29@users.noreply.github.com>
2023-01-02 10:59:46 +0530

    code practice #1 : Created demo pages - Navigation page, Tabbed page and Flyout page

---
+++ Kiran Workspace/MauiAppPractice/DemoPage.xaml.cs
@@ -0,0 +1,13 @@
+namespace MauiAppPractice;
+
+public partial class DemoPage : ContentPage
+{
+	public DemoPage()
+	{
+		InitializeComponent();
+	}
+	private async void Button_Clicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new MainPage());
+	}
+}
---
+++ Kiran Workspace/MauiAppPractice/FlyoutPageDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppPractice;
+
+public partial class FlyoutPageDemo : FlyoutPage
+{
+	public FlyoutPageDemo()
+	{
+		InitializeComponent();
+	}
+}
