9de3cf98e151987032b59e17dfea2879b11e2267
saya1605 <sayalikandhare16@hotmail.com>
2023-01-03 10:58:42 +0530

    Added KiranWorkspace

---
+++ KiranWorkspace/Dot Net Simple OOPs practice code/HelloWorld/PolyCar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class PolyCar : Baseclass
+    {
+        public override void mahindraCar()
+        {
+            Console.WriteLine("Thar");
+        }
+    }
+    class Polytruck : Baseclass
+    {
+        public override void mahindraCar()
+        {
+            Console.WriteLine("Choota Hatti");
+        }
+    }
+   
+}
