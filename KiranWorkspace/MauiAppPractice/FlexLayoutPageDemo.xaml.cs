1cc91b2efd62fda6b99ad3c8ee7465d0342f9261
KiranKhadd29 <121477204+KiranKhadd29@users.noreply.github.com>
2023-01-04 19:45:06 +0530

    code practice #1 - #Vid 4 : Absolute Page,FlexLayout Page & #Vid 5 : Presentation Control Page

---
+++ KiranWorkspace/MauiAppPractice/FlexLayoutPageDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppPractice;
+
+public partial class FlexLayoutPageDemo : ContentPage
+{
+	public FlexLayoutPageDemo()
+	{
+		InitializeComponent();
+	}
+}
--- KiranWorkspace/MauiAppPractice/MainPage.xaml.cs
+++ KiranWorkspace/MauiAppPractice/MainPage.xaml.cs
@@ -11,14 +11,15 @@ public partial class MainPage : ContentPage
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
+        Navigation.PushAsync(new TabbedPageDemo());
+        //count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        //if (count == 1)
+        //	CounterBtn.Text = $"Clicked {count} time";
+        //else
+        //	CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        //SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 }
 
