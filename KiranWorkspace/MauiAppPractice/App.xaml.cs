4eb3425120eaf74bce9a905abd842a1018b6973e
KiranKhadd29 <121477204+KiranKhadd29@users.noreply.github.com>
2023-01-06 15:58:51 +0530

    code practice #2: #Vid_08 - Activity Control & Collection Control Page - Added Main cs file changes

--- KiranWorkspace/MauiAppPractice/App.xaml.cs
+++ KiranWorkspace/MauiAppPractice/App.xaml.cs
@@ -19,6 +19,8 @@ public partial class App : Application
 		//MainPage = new FlexLayoutPageDemo();
 		//MainPage = new PresentationControlDemoPage();
 		//MainPage = new CommandControlDemoPage();
-        MainPage = new InputControlsDemoPage();
+        //MainPage = new InputControlsDemoPage();
+		//MainPage = new ActivityControlDemoPage();	
+		MainPage = new CollectionControlsDemoPage();	
     }
 }
