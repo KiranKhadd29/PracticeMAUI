2044405bfdc2a823577b0c2efa617b809e66ccd2
KiranKhadd29 <121477204+KiranKhadd29@users.noreply.github.com>
2023-01-17 13:11:12 +0530

    code practice #03 - #Vid_15

---
+++ KiranWorkspace/MauiAppPractice/MVVM/ViewModels/PersonViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MauiAppPractice.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MauiAppPractice.MVVM.ViewModels
+{
+    public class PersonViewModel
+    {
+        public Person Person { get; set; }
+        public PersonViewModel()
+        {
+            Person = new Person()
+            {
+                married = "I Am Married"
+
+            };
+        }            
+    }
+}
