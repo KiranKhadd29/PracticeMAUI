32d90fc83b76e9ac02d620c838af37c58cd953fb
KiranKhadd29 <121477204+KiranKhadd29@users.noreply.github.com>
2023-01-19 18:47:28 +0530

    code practice #03 - #Vid_17

--- KiranWorkspace/MauiAppPractice/MVVM/ViewModels/CommandViewModel.cs
+++ KiranWorkspace/MauiAppPractice/MVVM/ViewModels/CommandViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,14 +8,37 @@ using System.Windows.Input;
 
 namespace MauiAppPractice.MVVM.ViewModels
 {
+    [AddINotifyPropertyChangedInterface]
     public class CommandViewModel
     {
-        public ICommand CommandClick =>
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Result { get; set; }
+
+        public ICommand ResultCommand =>
+            new Command(Calculate);
+
+        private void Calculate(object obj)
+        {
+            Result = First + Second;
+        }
+
+        public ICommand CommandClick1 =>
             new Command(ExecuteAlert);
 
         private void ExecuteAlert(object obj)
         {
             App.Current.MainPage.DisplayAlert("Hi,", "Its Kiya Here", "Ok");
         }
+
+        public ICommand commandSearch =>
+            new Command(CommandPara);
+        private void CommandPara(object obj)
+        {
+            App.Current.MainPage.DisplayAlert("Hi,", $"{obj}", "Ok");
+        }
+
+        public string searchCommand { get; set; }
+
     }
 }
